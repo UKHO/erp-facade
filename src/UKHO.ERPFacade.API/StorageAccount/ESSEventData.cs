e311ab539ab7a5237bf2f77ceca8f30af5197c9b
Sakshi Singhal <sakshi15525@mastek.com>
2023-03-29 19:31:50 +0530

    74751- Added azure helper classes to extract trace Id and store event AB#74751 AB#75812 AB#75813 AB#75815 AB#75816

---
+++ src/UKHO.ERPFacade.API/StorageAccount/ESSEventData.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+
+namespace UKHO.ERPFacade.API.StorageAccount
+{
+    [Serializable]
+    public partial class ESSEventData
+    {
+        [JsonProperty("specversion")]
+        public string specversion { get; set; }
+
+        [JsonProperty("type")]
+        public string type { get; set; }
+
+        [JsonProperty("source")]
+        public string source { get; set; }
+
+        [JsonProperty("id")]
+        public string id { get; set; }
+
+        [JsonProperty("time")]
+        public string time { get; set; }
+
+        [JsonProperty("_COMMENT")]
+        public string _COMMENT { get; set; }
+
+        [JsonProperty("subject")]
+        public string subject { get; set; }
+
+        [JsonProperty("datacontenttype")]
+        public string datacontenttype { get; set; }
+
+        [JsonProperty("data")]
+        public DataObject data { get; set; }
+
+    }
+
+    [Serializable]
+    public partial class DataObject
+    {
+        [JsonProperty("traceId")]
+        public string traceId { get; set; }
+    }
+}
