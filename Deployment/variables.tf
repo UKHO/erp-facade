ef6a8a758ec12371898809042408f51073f2a40e
Amogh Lanke <amogh.lanke@mastek.conm>
2023-04-05 20:44:41 +0530

    74789 - added reference to tags variable in locals AB#74789 AB#75905

--- Deployment/variables.tf
+++ Deployment/variables.tf
@@ -13,12 +13,14 @@ locals {
   service_name       = "erpfacade"
   web_app_name       = "${local.service_name}-${local.env_name}-api"
   key_vault_name     = "${local.service_name}-ukho-${local.env_name}-kv"
+  tags = {
     SERVICE                   = "ERP Facade"
     ENVIRONMENT               = local.env_name
     SERVICE_OWNER             = "UKHO"
     RESPONSIBLE_TEAM          = "Mastek"
     CALLOUT_TEAM              = "On-Call_N/A"
     COST_CENTRE               = "011.05.12"
+    }
   }
 
 variable "sku_name" {
