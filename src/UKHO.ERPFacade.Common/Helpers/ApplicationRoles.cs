9b01426253f15354fcec8fd6ac5c523750712865
Shirin14926 <Shirin.talawdekar@mastek.com>
2023-04-18 11:40:58 +0530

    75462 add excludefromcodecoverage for model AB#75462

--- src/UKHO.ERPFacade.Common/Helpers/ApplicationRoles.cs
+++ src/UKHO.ERPFacade.Common/Helpers/ApplicationRoles.cs
@@ -1,6 +1,8 @@
-﻿
+﻿using System.Diagnostics.CodeAnalysis;
+
 namespace UKHO.ERPFacade.Common.Helpers
 {
+    [ExcludeFromCodeCoverage]
     public static class ApplicationRoles
     {
         public const string WebhookCallerRole = "WebhookCaller";
