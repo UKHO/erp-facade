7fd7ef405359590a8bcc8e15e3154bb8cc0554fc
Sadha1501493 <sadha1501493@mastek.com>
2023-04-18 15:23:06 +0530

    Revert "removed GetPriceInfo Endpoint"
    This reverts commit df8773ead2ef2c3d2a74693519c0fc0b2664bdb7.

---
+++ UKHO.ERPFacade.API.FunctionalTests/FunctionalTests/GetPriceInfoScenarios.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using UKHO.ERPFacade.API.FunctionalTests.Helpers;
+
+namespace UKHO.ERPFacade.API.FunctionalTests.FunctionalTests
+{
+    [TestFixture]
+    public class GetPriceInfoScenarios
+    {
+        [Test]
+        public void WhenValidEventInGetPriceInfo_ThenReturns200OkResponse()
+        {
+            var webhook = new GetPriceInfo();
+
+            var response = webhook.GetPriceInfoResponse();
+
+            Assert.That((int)response.StatusCode, Is.EqualTo(200));
+
+        }
+
+    }
+}
