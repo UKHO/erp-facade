b1f7ca3beb1f99e185955f2e4c3ae41a2ba30465
Prashant Gupta <prash1501461@mastek.com>
2023-04-21 17:01:47 +0530

    PBI 75462 with FTs

--- tests/UKHO.ERPFacade.API.FunctionalTests/Helpers/WebhookEndpoint.cs
+++ tests/UKHO.ERPFacade.API.FunctionalTests/Helpers/WebhookEndpoint.cs
@@ -13,22 +13,29 @@ namespace UKHO.ERPFacade.API.FunctionalTests.Helpers
             client = new RestClient(options);
         }
 
-        public async Task<RestResponse> OptionWebhookResponseAsync()
+        public async Task<RestResponse> OptionWebhookResponseAsync(String token)
         {
-            var request = new RestRequest("/webhook/newenccontentpublishedeventoptions");
-            var response = await client.OptionsAsync(request);
+            var request = new RestRequest("/webhook/newenccontentpublishedeventoptions", Method.Options);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Authorization", "Bearer " + token);
+            request.AddBody("{}");
+            RestResponse response = await client.ExecuteAsync(request);
             return response;
         }
 
-        public async Task<RestResponse> PostWebhookResponseAsync(String filePath)
+        public async Task<RestResponse> PostWebhookResponseAsync(String filePath, String token)
         {
             string requestBody;
             using (StreamReader streamReader = new StreamReader(filePath))
             {
                 requestBody = streamReader.ReadToEnd();
             }
-            var request = new RestRequest("/webhook/newenccontentpublishedeventreceived").AddJsonBody(requestBody);
-            var response = await client.PostAsync(request);
+            var request = new RestRequest("/webhook/newenccontentpublishedeventreceived", Method.Post);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Authorization", "Bearer " + token);
+            request.AddParameter("application/json", requestBody, ParameterType.RequestBody);
+
+            RestResponse response = await client.ExecuteAsync(request);
             return response;
         }
     }
