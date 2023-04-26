494d39db60fef61a3ae08ddf9b1a71fc691e1797
Nadeem Badai <Nadeem14456@Mastek.Com>
2023-04-26 14:27:10 +0530

    78336-deserialize model to XML AB#78433 AB#78434

---
+++ src/UKHO.ERPFacade.Common/IO/ObjectXMLSerializer.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using System.Xml.Serialization;
+using System.Xml;
+
+namespace UKHO.ERPFacade.Common.IO
+{
+    [ExcludeFromCodeCoverage]
+    public class ObjectXMLSerializer<T> where T : class
+    {
+        public static string SerializeObject(T serializableObject)
+        {
+            var serializer = new XmlSerializer(typeof(T));
+
+            var sb = new StringBuilder();
+
+            var settings = new XmlWriterSettings { Indent = true, };
+
+            using (var xw = XmlWriter.Create(sb, settings))
+            {
+                serializer.Serialize(xw, serializableObject);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
--- src/UKHO.SAP.MockAPIService/Services/z_adds_mat_info.cs
+++ src/UKHO.SAP.MockAPIService/Services/z_adds_mat_info.cs
@@ -1,4 +1,5 @@
 ﻿using System.Xml.Serialization;
+using UKHO.ERPFacade.Common.IO;
 using UKHO.SAP.MockAPIService.Models;
 
 namespace UKHO.SAP.MockAPIService.Services
@@ -8,6 +9,8 @@ namespace UKHO.SAP.MockAPIService.Services
         [return: XmlElement("Z_ADDS_MAT_INFOResponse", Namespace = "urn:sap-com:document:sap:rfc:functions")]
         public Z_ADDS_MAT_INFOResponse Z_ADDS_MAT_INFO([XmlElement("Z_ADDS_MAT_INFO", Namespace = "urn:sap-com:document:sap:rfc:functions")] Z_ADDS_MAT_INFO z_ADDS_MAT_INFO)
         {
+            string xml = ObjectXMLSerializer<Z_ADDS_MAT_INFO>.SerializeObject(z_ADDS_MAT_INFO);
+
             return new Z_ADDS_MAT_INFOResponse()
             {
                 EX_MESSAGE = "Record successfully received for " + z_ADDS_MAT_INFO.IM_MATINFO.CORRID,
