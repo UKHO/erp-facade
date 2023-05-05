1fa6fae8fe473e57cf9c903f7d95c896a5647319
Vishal Dukare <Vishal14583@Mastek.Com>
2023-05-05 10:59:21 +0530

    77823-minor changes in config

--- tests/UKHO.ERPFacade.API.FunctionalTests/FunctionalTests/WebhookScenarios.cs
+++ tests/UKHO.ERPFacade.API.FunctionalTests/FunctionalTests/WebhookScenarios.cs
@@ -7,48 +7,42 @@ namespace UKHO.ERPFacade.API.FunctionalTests.FunctionalTests
     [TestFixture]
     public class WebhookScenarios
     {
-        private WebhookEndpoint Webhook { get; set; }
-        private DirectoryInfo _dir;
-        private readonly ADAuthTokenProvider _authToken = new ADAuthTokenProvider();
-        public static Boolean noRole = false;
+        private WebhookEndpoint _webhook;
+        private readonly ADAuthTokenProvider _authToken = new();
+
+        private static readonly string _webhookPayloadFilePath = "ERPFacadePayloadTestData/WebhookPayload.json";
 
         [SetUp]
         public void Setup()
         {
-            Webhook = new WebhookEndpoint();
-            _dir = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent;
+            _webhook = new WebhookEndpoint();
         }
 
         [Test(Description = "WhenValidEventInNewEncContentPublishedEventOptions_ThenWebhookReturns200OkResponse"), Order(0)]
         public async Task WhenValidEventInNewEncContentPublishedEventOptions_ThenWebhookReturns200OkResponse()
         {
-            var response = await Webhook.OptionWebhookResponseAsync(await _authToken.GetAzureADToken(false));
+            var response = await _webhook.OptionWebhookResponseAsync(await _authToken.GetAzureADToken(false));
             response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
         }
 
         [Test(Description = "WhenValidEventInNewEncContentPublishedEventReceivedWithValidToken_ThenWebhookReturns200OkResponse"), Order(0)]
         public async Task WhenValidEventInNewEncContentPublishedEventReceivedWithValidToken_ThenWebhookReturns200OkResponse()
         {
-            string filePath = Path.Combine(_dir.FullName, WebhookEndpoint.config.testConfig.PayloadFolder, WebhookEndpoint.config.testConfig.WebhookPayloadFileName);
-
-            var response = await Webhook.PostWebhookResponseAsync(filePath, await _authToken.GetAzureADToken(false));
+            var response = await _webhook.PostWebhookResponseAsync(_webhookPayloadFilePath, await _authToken.GetAzureADToken(false));
             response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
         }
 
         [Test(Description = "WhenValidEventInNewEncContentPublishedEventReceivedWithInvalidToken_ThenWebhookReturns401UnAuthorizedResponse"), Order(1)]
         public async Task WhenValidEventInNewEncContentPublishedEventReceivedWithInvalidToken_ThenWebhookReturns401UnAuthorizedResponse()
         {
-            string filePath = Path.Combine(_dir.FullName, WebhookEndpoint.config.testConfig.PayloadFolder, WebhookEndpoint.config.testConfig.WebhookPayloadFileName);
-
-            var response = await Webhook.PostWebhookResponseAsync(filePath, "invalidToken_abcd");
+            var response = await _webhook.PostWebhookResponseAsync(_webhookPayloadFilePath, "invalidToken_abcd");
             response.StatusCode.Should().Be(System.Net.HttpStatusCode.Unauthorized);
         }
 
         [Test(Description = "WhenValidEventInNewEncContentPublishedEventReceivedWithTokenHavingNoRole_ThenWebhookReturns403ForbiddenResponse"), Order(1)]
         public async Task WhenValidEventInNewEncContentPublishedEventReceivedWithTokenHavingNoRole_ThenWebhookReturns403ForbiddenResponse()
         {
-            string filePath = Path.Combine(_dir.FullName, WebhookEndpoint.config.testConfig.PayloadFolder, WebhookEndpoint.config.testConfig.WebhookPayloadFileName);
-            var response = await Webhook.PostWebhookResponseAsync(filePath, await _authToken.GetAzureADToken(true));
+            var response = await _webhook.PostWebhookResponseAsync(_webhookPayloadFilePath, await _authToken.GetAzureADToken(true));
             response.StatusCode.Should().Be(System.Net.HttpStatusCode.Forbidden);
         }
     }
