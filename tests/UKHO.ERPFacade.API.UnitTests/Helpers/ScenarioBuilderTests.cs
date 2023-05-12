67b52d0844c55e22e90fe1f7ee909479a66c8fcf
mayuresh619 <56475636+mayuresh619@users.noreply.github.com>
2023-05-12 12:30:32 +0530

    77802 - Update cell scenario implemenation. AB#77802 AB#79923

--- tests/UKHO.ERPFacade.API.UnitTests/Helpers/ScenarioBuilderTests.cs
+++ tests/UKHO.ERPFacade.API.UnitTests/Helpers/ScenarioBuilderTests.cs
@@ -323,5 +323,76 @@ namespace UKHO.ERPFacade.API.UnitTests.Helpers
              && call.GetArgument<IEnumerable<KeyValuePair<string, object>>>(2)!.ToDictionary(c => c.Key, c => c.Value)["{OriginalFormat}"].ToString() == "Scenario identified for {ProductName} is {ScenarioName}.").MustHaveHappenedOnceExactly();
         }
 
+        [Test]
+        public void WhenEesEventDataPassedWithStatusNameAsUpdateAndContentChangedAsTrue_ThenReturnsUpdateScenario()
+        {
+            var fakeEventData = new EESEvent()
+            {
+                Data = new Data()
+                {
+                    Products = new List<Product>()
+                    {
+                        new Product()
+                        {
+                            ReplacedBy = new List<string> { "Cell2", "Cell3" },
+                            InUnitsOfSale = new List<string> { "Cell1", "UnitOfSale1", "UnitofSale2" },
+                            ProductName = "Cell1",
+                            Status = new Status()
+                            {
+                            IsNewCell= false,
+                            StatusName="Update"
+                            },
+                            ContentChanged=true
+                        }
+                    },
+                    UnitsOfSales = new List<UnitOfSale>()
+                    {
+                        new UnitOfSale()
+                        {
+                            UnitName = "Cell1",
+                            CompositionChanges = new CompositionChanges()
+                            {
+                                AddProducts = new List<string> { "Cell2", "Cell3" },
+                                RemoveProducts = new List<string> { "Cell1" }
+                            }
+                        },
+                        new UnitOfSale()
+                        {
+                            UnitName = "UnitOfSale1",
+                            CompositionChanges = new CompositionChanges()
+                            {
+                                AddProducts = new List<string> { "Cell2", "Cell3" },
+                                RemoveProducts = new List<string> { "Cell1" }
+                            }
+                        },
+                        new UnitOfSale()
+                        {
+                            UnitName = "UnitOfSale2",
+                            CompositionChanges = new CompositionChanges()
+                            {
+                                AddProducts = new List<string> { "Cell2", "Cell3" },
+                                RemoveProducts = new List<string> { "Cell1" }
+                            }
+                        }
+                    },
+                }
+            };
+
+            var result = _fakeScenarioBuilder.BuildScenarios(fakeEventData);
+
+            result.Count().Should().BeGreaterThan(0);
+            result.Should().Satisfy(scenario => scenario.ScenarioType == ScenarioType.UpdateCell);
+
+            A.CallTo(_fakeLogger).Where(call => call.Method.Name == "Log"
+             && call.GetArgument<LogLevel>(0) == LogLevel.Information
+             && call.GetArgument<EventId>(1) == EventIds.IdentifyScenarioStarted.ToEventId()
+             && call.GetArgument<IEnumerable<KeyValuePair<string, object>>>(2)!.ToDictionary(c => c.Key, c => c.Value)["{OriginalFormat}"].ToString() == "Identifying the scenarios based on received ENC content publish event.").MustHaveHappenedOnceExactly();
+
+            A.CallTo(_fakeLogger).Where(call => call.Method.Name == "Log"
+             && call.GetArgument<LogLevel>(0) == LogLevel.Information
+             && call.GetArgument<EventId>(1) == EventIds.ScenarioIdentified.ToEventId()
+             && call.GetArgument<IEnumerable<KeyValuePair<string, object>>>(2)!.ToDictionary(c => c.Key, c => c.Value)["{OriginalFormat}"].ToString() == "Scenario identified for {ProductName} is {ScenarioName}.").MustHaveHappenedOnceExactly();
+        }
+
     }
 }
