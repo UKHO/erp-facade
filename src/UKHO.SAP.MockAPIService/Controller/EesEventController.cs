506377d4b3fb635259b810c0f5c4a772e6eca6c4
Shirin14926 <Shirin.talawdekar@mastek.com>
2023-05-16 15:05:35 +0530

    79664 code change-create http api endpoint AB#80051 AB#79664

---
+++ src/UKHO.SAP.MockAPIService/Controller/EesEventController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace UKHO.SAP.MockAPIService.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EesEventController : ControllerBase
+    {
+        [HttpPost]
+        [Route("/api/events")]
+        public virtual async Task<IActionResult> Post([FromBody] JObject eventJson)
+        {
+            //store event in blob container
+            await Task.CompletedTask;
+            return new OkObjectResult(StatusCodes.Status200OK); //if test case set to 401-Unauthorized then 401
+        }
+    }
+}
