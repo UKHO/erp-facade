98959bb98d40a8214f46bf211ebd9e2336a52da9
mayur1501364 <127309264+mayur1501364@users.noreply.github.com>
2023-06-01 19:44:39 +0530

    82164 - created skeleton for PublishPriceChange WebJob AB#82164

---
+++ src/UKHO.ERPFacade.PublishPriceChange.WebJob/PublishPriceChangeWebJob.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Diagnostics.CodeAnalysis;
+
+namespace UKHO.ERPFacade.PublishPriceChange.WebJob
+{
+    [ExcludeFromCodeCoverage]
+    public class PublishPriceChangeWebJob
+    {
+        private readonly ILogger<PublishPriceChangeWebJob> _logger;
+
+        public PublishPriceChangeWebJob(ILogger<PublishPriceChangeWebJob> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public void Start()
+        {
+            //Add code here
+            Console.WriteLine(nameof(PublishPriceChangeWebJob));
+        }
+    }
+}
