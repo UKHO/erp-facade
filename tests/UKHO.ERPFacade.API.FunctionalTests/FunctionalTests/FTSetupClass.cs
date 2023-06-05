1fe3d2469f3c393922248cc873891292744168a9
Sadha1501493 <sadha1501493@mastek.com>
2023-06-05 17:46:58 +0530

    Added  SetUpFixture for FT Added category for UoS and Price Change end point

---
+++ tests/UKHO.ERPFacade.API.FunctionalTests/FunctionalTests/FTSetupClass.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UKHO.ERPFacade.API.FunctionalTests.Helpers;
+
+namespace UKHO.ERPFacade.API.FunctionalTests.FunctionalTests
+{
+    
+    [SetUpFixture]
+    internal class FTSetupClass
+    {
+        
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            Config.ConfigSetup();
+            Console.WriteLine("Inside FTSetupClass");
+
+        }
+    }
+}
--- tests/UKHO.ERPFacade.API.FunctionalTests/FunctionalTests/UnitOfSaleScenarios.cs
+++ tests/UKHO.ERPFacade.API.FunctionalTests/FunctionalTests/UnitOfSaleScenarios.cs
@@ -9,7 +9,6 @@ namespace UKHO.ERPFacade.API.FunctionalTests.FunctionalTests
     {
 
         private UnitOfSaleEndpoint _unitOfSale { get; set; }
-        public static Config config;
         private readonly ADAuthTokenProvider _authToken = new();
         public static bool noRole = false;
         //for pipeline
@@ -20,31 +19,36 @@ namespace UKHO.ERPFacade.API.FunctionalTests.FunctionalTests
         [OneTimeSetUp]
         public void Setup()
         {
-            config = new();
-            _unitOfSale = new UnitOfSaleEndpoint(config.TestConfig.ErpFacadeConfiguration.BaseUrl);
+            _unitOfSale = new UnitOfSaleEndpoint(Config.TestConfig.ErpFacadeConfiguration.BaseUrl);
         }
-       
+
+        [Category("DevEnvFT")]
+        [Category("QAEnvFT")]
         [Test(Description = "WhenValidEventReceivedWithValidToken_ThenUoSReturns200OkResponse"), Order(0)]
         public async Task WhenValidEventReceivedWithValidToken_ThenUoSReturns200OkResponse()
         {
-            string filePath = Path.Combine(_projectDir, config.TestConfig.PayloadFolder, config.TestConfig.UoSPayloadFileName);            
+            string filePath = Path.Combine(_projectDir, Config.TestConfig.PayloadFolder, Config.TestConfig.UoSPayloadFileName);            
             var response = await _unitOfSale.PostUoSResponseAsync(filePath, await _authToken.GetAzureADToken(false, "UnitOfSale"));
            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
 
         }
+        [Category("DevEnvFT")]
+        [Category("QAEnvFT")]
         [Test(Description = "WhenValidEventReceivedWithInvalidToken_ThenUoSReturns401UnAuthorizedResponse"), Order(1)]
         public async Task WhenValidEventReceivedWithInvalidToken_ThenUoSReturns401UnAuthorizedResponse()
         {
-            string filePath = Path.Combine(_projectDir, config.TestConfig.PayloadFolder, config.TestConfig.UoSPayloadFileName);
+            string filePath = Path.Combine(_projectDir, Config.TestConfig.PayloadFolder, Config.TestConfig.UoSPayloadFileName);
 
             var response = await _unitOfSale.PostUoSResponseAsync(filePath, "invalidToken_abcd");
             response.StatusCode.Should().Be(System.Net.HttpStatusCode.Unauthorized);
 
         }
+        [Category("DevEnvFT")]
+        [Category("QAEnvFT")]
         [Test(Description = "WhenValidEventReceivedWithTokenHavingNoRole_ThenUoSReturns403ForbiddenResponse"), Order(2)]
         public async Task WhenValidEventReceivedWithInvalidToken_ThenUoSReturns403UnAuthorizedResponse()
         {
-            string filePath = Path.Combine(_projectDir, config.TestConfig.PayloadFolder, config.TestConfig.UoSPayloadFileName);
+            string filePath = Path.Combine(_projectDir, Config.TestConfig.PayloadFolder, Config.TestConfig.UoSPayloadFileName);
 
             var response = await _unitOfSale.PostUoSResponseAsync(filePath, await _authToken.GetAzureADToken(true));
             response.StatusCode.Should().Be(System.Net.HttpStatusCode.Forbidden);
