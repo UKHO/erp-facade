8f6065469f67c3c80bde2f92c5898718571020bd
Amogh Lanke <amogh.lanke@mastek.conm>
2023-06-10 00:23:32 +0530

    81054 changed env name variable to locals value AB#81054

--- Deployment/main.tf
+++ Deployment/main.tf
@@ -31,7 +31,7 @@ data "azurerm_subnet" "main_subnet" {
     location                  = azurerm_resource_group.rg.location
     sku_name                  = var.sku_name[local.env_name]
     subnet_id                 = data.azurerm_subnet.main_subnet.id
-    allowed_ips               = (var.env_name  == "dev") ? "" : var.allowed_ips
+    allowed_ips               = (local.env_name  == "dev") ? "" : var.allowed_ips
     app_settings = {
       "KeyVaultSettings:ServiceUri"                              = "https://${local.key_vault_name}.vault.azure.net/"
       "EventHubLoggingConfiguration:Environment"                 = local.env_name
