c018708a41162dd3ba91393ebd46bf7ec77a4e59
Amogh Lanke <amogh.lanke@mastek.conm>
2023-06-13 13:14:30 +0530

    81054 tweaked Propertyfor action in IP restriction block AB#81054

--- Deployment/Modules/Webapp/main.tf
+++ Deployment/Modules/Webapp/main.tf
@@ -24,6 +24,7 @@ resource "azurerm_windows_web_app" "webapp_service" {
     ftps_state = "Disabled"
 
     dynamic "ip_restriction" {
+      action = allow
       for_each = var.allowed_ips
       content {
           ip_address  = length(split("/",ip_restriction.value)) > 1 ? ip_restriction.value : "${ip_restriction.value}/32"
