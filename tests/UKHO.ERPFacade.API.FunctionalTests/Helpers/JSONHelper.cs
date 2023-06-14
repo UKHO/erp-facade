3074382da5ec12f33268a06ec58329fbb05b5f76
Sadha1501493 <sadha1501493@mastek.com>
2023-06-14 15:40:07 +0530

    79667-Added JSON helper & folder structure for product JSONAB#79667

---
+++ tests/UKHO.ERPFacade.API.FunctionalTests/Helpers/JSONHelper.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UKHO.ERPFacade.API.FunctionalTests.Model;
+using UKHO.ERPFacade.Common.Models;
+
+namespace UKHO.ERPFacade.API.FunctionalTests.Helpers
+{
+
+
+    public class JSONHelper
+    {
+        public string getDeserializedString(String filePath)
+        {
+
+            string requestBody;
+
+            using (StreamReader streamReader = new StreamReader(filePath))
+            {
+                requestBody = streamReader.ReadToEnd();
+            }
+            return requestBody;
+        }
+    }
+}
