1fd50a70d6dc442ca3ca7f09365f0859c65f26bf
Nadeem Badai <Nadeem14456@Mastek.Com>
2023-07-13 20:33:53 +0530

    85650-code changes for Interactive token provider AB#85650

--- src/UKHO.ERPFacade.Common/Infrastructure/Config/ErpPublishEventSource.cs
+++ src/UKHO.ERPFacade.Common/Infrastructure/Config/ErpPublishEventSource.cs
@@ -8,6 +8,7 @@ namespace UKHO.ERPFacade.Common.Infrastructure.Config
         public string ServiceUrl { get; set; }
         public string PublishEndpoint { get; set; }
         public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
         public string PublisherScope { get; set; }
         public string ApplicationUri { get; set; }
     }
