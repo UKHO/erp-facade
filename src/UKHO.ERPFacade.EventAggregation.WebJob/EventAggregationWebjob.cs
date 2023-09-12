7e1ba15160deae060aa332328abe15f0bd08e643
Sakshi Singhal <sakshi15525@mastek.com>
2023-09-12 18:15:41 +0530

    91583- Added webjob configuration AB#91583 AB#93345

---
+++ src/UKHO.ERPFacade.EventAggregation.WebJob/EventAggregationWebjob.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Azure.Storage.Queues.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using UKHO.ERPFacade.EventAggregation.WebJob.Services;
+
+namespace UKHO.ERPFacade.EventAggregation.WebJob
+{
+    [ExcludeFromCodeCoverage]
+    public class EventAggregationWebjob
+    {
+        private readonly ILogger<EventAggregationWebjob> _logger;
+        private readonly IAggregationService _aggregationService;
+
+        public EventAggregationWebjob(ILogger<EventAggregationWebjob> logger, IAggregationService aggregationService)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _aggregationService = aggregationService ?? throw new ArgumentNullException(nameof(aggregationService));
+        }
+        public Task ProcessQueueMessage([QueueTrigger("recordofsaleevents")] QueueMessage message)
+        {
+            _aggregationService.MergeRecordOfSaleEvents(message);
+
+            return Task.CompletedTask;
+        }
+    }
+}
