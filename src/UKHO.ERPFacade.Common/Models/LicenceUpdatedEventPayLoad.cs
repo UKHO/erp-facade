5f00462cfdf5f3787f803f040194af20a438bf8a
Sakshi Singhal <sakshi15525@mastek.com>
2023-10-31 16:02:48 +0530

    98200- Changed attribute ecdisManufacturerId to ecdisManufacturerName AB#97880 AB#98410

--- src/UKHO.ERPFacade.Common/Models/LicenceUpdatedEventPayLoad.cs
+++ src/UKHO.ERPFacade.Common/Models/LicenceUpdatedEventPayLoad.cs
@@ -95,8 +95,8 @@ namespace UKHO.ERPFacade.Common.Models
         [JsonProperty("numberLicenceUsers")]
         public int? NumberLicenceUsers { get; set; }
 
-        [JsonProperty("ecdisManufacturerId")]
-        public string EcdisManufacturerId { get; set; }
+        [JsonProperty("ecdisManufacturerName")]
+        public string EcdisManufacturerName { get; set; }
 
         [JsonProperty("licenceDuration")]
         public int? LicenceDuration { get; set; }
