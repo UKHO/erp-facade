48b350ce49bfb20edf140ebb3fd7cd3951932c94
Harshda Joshi <harshda15125@mastek.com>
2024-01-08 12:17:02 +0530

    107519 - Code refactored AB#107519

--- src/UKHO.ERPFacade.EventAggregation.WebJob/Helpers/RecordOfSaleSapMessageBuilder.cs
+++ src/UKHO.ERPFacade.EventAggregation.WebJob/Helpers/RecordOfSaleSapMessageBuilder.cs
@@ -312,7 +312,7 @@ namespace UKHO.ERPFacade.EventAggregation.WebJob.Helpers
         {
             SapRecordOfSalePayLoad rosConvertLicencePayload = new();
 
-            foreach (RecordOfSaleEventPayLoad eventData in eventDataList)
+            foreach (var eventData in eventDataList)
             {
                 if (rosConvertLicencePayload.PROD == null!)
                 {
