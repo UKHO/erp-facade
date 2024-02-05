cc469a2716a8ab150e7a160045caae4cf23aeaca
John Rippington <john.rippington@ukho.gov.uk>
2024-02-05 09:22:32 +0000

    Revert "New environment markup"
    This reverts commit 14d3d7e4d68194299754daeea84fa8674aa3e35a.

--- Deployment/variables.tf
+++ Deployment/variables.tf
@@ -31,8 +31,6 @@ variable "sku_name" {
   type = map(any)
   default = {
              "dev"  =  "P1v2"
-			 "iat"   =  "P1v3"
-             "e2e"   =  "P1v3"
              "qa"   =  "P1v3"
              live   =  "P1v3"
             }
