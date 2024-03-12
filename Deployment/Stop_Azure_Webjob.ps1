5def518ff7d2fd0e83f422434b1cb169ec14b6cf
Vinod Nerella <vinod1502506@mastek.com>
2024-03-12 17:43:42 +0530

    145255 test azurecli aggregator webjob AB#145255

--- Deployment/Stop_Azure_Webjob.ps1
+++ Deployment/Stop_Azure_Webjob.ps1
@@ -8,5 +8,9 @@ Write-Output "Stopping event aggregator webjob"
 Write-Output "az webapp webjob continuous stop --name $appname --resource-group $resourcegroup --webjob-name EventAggregationWebJob"
 Write-Output "az webapp webjob continuous list --name $appname --resource-group $resourcegroup"
 az webapp webjob continuous list --name $appname --resource-group $resourcegroup
+az webapp config appsettings set -g $resourcegroup -n $appname --settings WEBSITE_RUN_FROM_PACKAGE=0
+az webapp restart --name $appname --resource-group $resourcegroup
 Write-Output "Stopping Webjob"
 az webapp webjob continuous stop --name $appname --resource-group $resourcegroup --webjob-name EventAggregationWebJob
+az webapp config appsettings set -g $resourcegroup -n $appname --settings WEBSITE_RUN_FROM_PACKAGE=1
+az webapp restart --name $appname --resource-group $resourcegroup
