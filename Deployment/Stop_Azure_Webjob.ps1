39000bd506fbc4c8a7e606cfe730a3d51d889455
amogh14893 <amogh.lanke@mastek.com>
2024-03-12 11:14:27 +0530

    to print list of webjobs

--- Deployment/Stop_Azure_Webjob.ps1
+++ Deployment/Stop_Azure_Webjob.ps1
@@ -6,5 +6,7 @@ param (
 
 Write-Output "Stopping event aggregator webjob"
 Write-Output "az webapp webjob continuous stop --name $appname --resource-group $resourcegroup --webjob-name EventAggregationWebJob"
+Write-Output "az webapp webjob continuous list --name $appname --resource-group $resourcegroup"
+az webapp webjob continuous list --name $appname --resource-group $resourcegroup
 az webapp webjob continuous stop --name $appname --resource-group $resourcegroup --webjob-name EventAggregationWebJob
 az webapp restart --name $appname --resource-group $resourcegroup
