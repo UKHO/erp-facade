17e3726d64c527f0c877ababdec548e141f78ceb
Sadha1501493 <sadha1501493@mastek.com>
2024-03-21 18:19:12 +0530

    Revert "148807 removed extra line from maintf file"
    This reverts commit f35e3097a33ff107e6079d84beee75dd054f8301.

--- Deployment/main.tf
+++ Deployment/main.tf
@@ -40,7 +40,8 @@ module "webapp_service" {
     "ASPNETCORE_ENVIRONMENT"                                   = local.env_name
     "WEBSITE_RUN_FROM_PACKAGE"                                 = "1"
     "WEBSITE_ENABLE_SYNC_UPDATE_SITE"                          = "true"
-    "WEBSITE_ADD_SITENAME_BINDINGS_IN_APPHOST_CONFIG"          = "1"    
+    "WEBSITE_ADD_SITENAME_BINDINGS_IN_APPHOST_CONFIG"          = "1"
+    
   }
   mock_app_settings = {
     "KeyVaultSettings:ServiceUri"                              = "https://${local.key_vault_name}.vault.azure.net/"
