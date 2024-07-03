f4f29f715d88c738de767cca8f25f48f1a84b7ff
John Rippington <john.rippington@ukho.gov.uk>
2024-07-03 15:28:59 +0100

    Removed duplicate

--- Deployment/azure.tf
+++ Deployment/azure.tf
@@ -1,22 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.89.0"
-      configuration_aliases = [ # merged from pvt ep module
-        azurerm.hub,
-        azurerm.spoke,
-      ]
-    }
-  }
-
-  required_version = "=1.7.2"
-  backend "azurerm" {
-    container_name = "tfstate"
-    key            = "terraform.deployment.tfplan"
-  }
-}
-
 locals {
   hub_subscription_id = "282900b8-5415-4137-afcc-fd13fe9a64a7"
   subscription_id = var.subscription_id
