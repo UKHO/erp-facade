c00f0052bd29e64e46ea7ee20601a50aa6792897
John Rippington <john.rippington@ukho.gov.uk>
2024-07-03 16:37:39 +0100

    Update

--- Deployment/azure.tf
+++ Deployment/azure.tf
@@ -4,7 +4,7 @@ provider "azurerm" {
 
 locals {
   hub_subscription_id = "282900b8-5415-4137-afcc-fd13fe9a64a7"
-  subscription_id = var.subscription_id
+  subscription_id = "a3b2d2c4-28e8-4e1c-98c3-9804de5561b4"
 }
 
 provider "azurerm" {
