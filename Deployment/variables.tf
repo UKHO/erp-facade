9cab6e627802815769eb40be61136d7cda5818a2
rushdynajath <rushdy.najath@ukho.gov.uk>
2024-07-09 16:49:24 +0100

    chore: Update sku_name variable in Deployment/variables.tf and rename "preprod" to "prp"

--- Deployment/variables.tf
+++ Deployment/variables.tf
@@ -34,7 +34,7 @@ variable "sku_name" {
             "iat"     =  "P1v3"            
             "e2e"     =  "P1v3"
             "qa"      =  "P1v3"
-            "preprod" =  "P1v3"
+            "prp"     =  "P1v3"
             live      =  "P1v3"
             }
 }
