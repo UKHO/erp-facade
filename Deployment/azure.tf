a5840558f111411985b865a39f3f6b5cedc43141
John Rippington <john@portolan.co>
2024-07-11 07:31:52 +0100

    Updated backend config

--- Deployment/azure.tf
+++ Deployment/azure.tf
@@ -1,15 +1,12 @@
 terraform {
   backend "azurerm" {
+    key                  = "terraform.deployment.tfplan"
     container_name       = "tfstate"
   }
 }
 
 provider "azurerm" {
   features {}
-}
-
-locals {
-  
 }
 
 provider "azurerm" {
