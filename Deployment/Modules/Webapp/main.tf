012ab581c3227b7bb23fd820b6ddc6d00fac8e60
rushdynajath <rushdy.najath@ukho.gov.uk>
2024-08-19 10:16:07 +0100

    add mock webapp service

--- Deployment/Modules/Webapp/main.tf
+++ Deployment/Modules/Webapp/main.tf
@@ -98,6 +98,11 @@ resource "azurerm_app_service_virtual_network_swift_connection" "webapp_vnet_int
   subnet_id      = var.subnet_id
 }
 
+resource "azurerm_app_service_virtual_network_swift_connection" "mock_webapp_vnet_integration" {
+  app_service_id = azurerm_windows_web_app.mock_webapp_service.id
+  subnet_id      = var.subnet_id
+}
+
 resource "azurerm_app_service_slot_virtual_network_swift_connection" "slot_vnet_integration" {
   app_service_id = azurerm_windows_web_app.webapp_service.id
   subnet_id      = var.subnet_id
