aa58f120c947ffb6d1c1fb4aa1968e50df300966
rushikeshkhopatkar <rushike12794@mastek.com>
2024-10-01 17:14:02 +0530

    177261 - Updated existing FTs' as per new rules and new payload schema. Added new FT's to validate mandatory attributes in the payload. AB#177261

--- tests/UKHO.ERPFacade.API.FunctionalTests/FunctionalTests/WebhookScenarios.cs
+++ tests/UKHO.ERPFacade.API.FunctionalTests/FunctionalTests/WebhookScenarios.cs
@@ -1,9 +1,13 @@
-﻿using FluentAssertions;
+﻿using System;
+using FluentAssertions;
+using Newtonsoft.Json;
 using NUnit.Framework;
 using RestSharp;
 using UKHO.ERPFacade.API.FunctionalTests.Configuration;
 using UKHO.ERPFacade.API.FunctionalTests.Helpers;
+using UKHO.ERPFacade.API.FunctionalTests.Model;
 using UKHO.ERPFacade.API.FunctionalTests.Service;
+using UKHO.ERPFacade.Common.Models;
 
 namespace UKHO.ERPFacade.API.FunctionalTests.FunctionalTests
 {
@@ -12,6 +16,7 @@ namespace UKHO.ERPFacade.API.FunctionalTests.FunctionalTests
     {
         private WebhookEndpoint WebhookEndpoint { get; set; }
         private readonly ADAuthTokenProvider _authToken = new();
+
         private readonly string _projectDir = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory));
         //for local
         //private readonly string _projectDir = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, "..\\..\\.."));
@@ -55,74 +60,92 @@ namespace UKHO.ERPFacade.API.FunctionalTests.FunctionalTests
 
         [Test, Order(1)]
         //New Cell
-        [TestCase("ID3_1NewCellScenario.JSON", "Y", "SamePermitKey", TestName = "WhenICallTheWebhookWithOneNewCellScenario_ThenWebhookReturns200Response")]
-        [TestCase("ID4_2NewCellScenario.JSON", "N", "DifferentPermitKey", TestName = "WhenICallTheWebhookWithTwoNewCellScenario_ThenWebhookReturns200Response")]
-        [TestCase("ID5_1NewCellWoNewAVCSUnit.JSON", "Y", "SamePermitKey", TestName = "WhenICallTheWebhookWithOneNewCellScenarioWithourAVCSUoS_ThenWebhookReturns200Response")]
+        [TestCase("NewCell.JSON", "PermitWithSameKey", TestName = "WhenICallTheWebhookWithNewCellScenario_ThenWebhookReturns200Response")]
+        [TestCase("2NewCells.JSON", "PermitWithDifferentKey", TestName = "WhenICallTheWebhookWith2NewCellsScenario_ThenWebhookReturns200Response")]
 
-        //Cancel & Replace
-        [TestCase("ID6_2CellsReplace1CellsCancel.JSON", "N", "SamePermitKey", TestName = "WhenICallTheWebhookWithTwoCellsReplacesOneCellScenario_ThenWebhookReturns200Response")]
-        [TestCase("ID7_1CellCancel.JSON", "N", TestName = "WhenICallTheWebhookWithOneCancelCellScenario_ThenWebhookReturns200Response")]
+        //Additional Coverage
+        [TestCase("3AdditionalCoverageCell.JSON", "PermitWithSameKey", TestName = "WhenICallTheWebhookWith3AdditionalCoverageCellScenario_ThenWebhookReturns200Response")]
+        [TestCase("3AdditionalCoverageCell_AND_CancelCellWithNewCellReplacement.JSON", "PermitWithDifferentKey", TestName = "WhenICallTheWebhookWith3AdditionalCoverageCell_AND_CancelCellWithNewCellReplacementScenario_ThenWebhookReturns200Response")]
+        [TestCase("AdditionalCoverageWithNewEdition.JSON", "PermitWithSameKey", TestName = "WhenICallTheWebhookWithAdditionalCoverageWithNewEditionScenario_ThenWebhookReturns200Response")]
+
+        //Cancel and Replace
+        [TestCase("CancelCellWithExistingCellReplacement.JSON", "PermitWithSameKey", TestName = "WhenICallTheWebhookWithCancelCellWithExistingCellReplacementScenario_ThenWebhookReturns200Response")]
+        [TestCase("CancelCellWithNewCellReplacement.JSON", "PermitWithDifferentKey", TestName = "WhenICallTheWebhookWithCancelCellWithNewCellReplacementScenario_ThenWebhookReturns200Response")]
+        [TestCase("CancelCellWithNewCellReplacement_AND_CellMetadataChange_AND_MoveCell.JSON", "PermitWithSameKey", TestName = "WhenICallTheWebhookWithCancelCellWithNewCellReplacement_AND_CellMetadataChange_AND_MoveCellScenario_ThenWebhookReturns200Response")]
+        [TestCase("CancelCellWithNewCellReplacement_AND_UpdateCell.JSON", "PermitWithSameKey", TestName = "WhenICallTheWebhookWithCancelCellWithNewCellReplacement_AND_UpdateCellScenario_ThenWebhookReturns200Response")]
+        [TestCase("CancelCellWithoutCellReplacement.JSON", "PermitWithSameKey", TestName = "WhenICallTheWebhookWithCancelCellWithoutCellReplacementScenario_ThenWebhookReturns200Response")]
 
         //Metadata Change
-        [TestCase("ID8_2CellMetadataChange.JSON", "Y", TestName = "WhenICallTheWebhookWith2CellMetadataChangeScenario_ThenWebhookReturns200Response")]
-        [TestCase("ID9_MetadataChange.JSON", "N", TestName = "WhenICallTheWebhookWithMetadataChangeScenario_ThenWebhookReturns200Response")]
+        [TestCase("CellMetadataChange.JSON", "PermitWithSameKey", TestName = "WhenICallTheWebhookWithCellMetadataChangeScenario_ThenWebhookReturns200Response")]
+        [TestCase("UoSMetadataChange.JSON", "PermitWithSameKey", TestName = "WhenICallTheWebhookWithUoSMetadataChangeScenario_ThenWebhookReturns200Response")]
 
-        //Update
-        [TestCase("ID10_UpdateSimple.JSON", "Y", "SamePermitKey", TestName = "WhenICallTheWebhookWithSimpleUpdateScenarioHavingOneCellWithStatusNameAsUpdate_ThenWebhookReturns200Response")]
-        [TestCase("ID11_updateOneCellWithNewEditionStatus.JSON", "N", "SamePermitKey", TestName = "WhenICallTheWebhookWithSimpleUpdateScenarioHavingOneCellWithStatusNameAsNewEdition_ThenWebhookReturns200Response")]
-        [TestCase("ID12_updateOneCellWithReIssueStatus.JSON", "N", "SamePermitKey", TestName = "WhenICallTheWebhookWithSimpleUpdateScenarioHavingOneCellStatusNameAsReIssue_ThenWebhookReturns200Response")]
-        [TestCase("ID13_updateTwoCellsWithDifferentStatusName.JSON", "N", "DifferentPermitKey", TestName = "WhenICallTheWebhookWithSimpleUpdateScenarioHavingTwoCellsWithDifferentStatusName_ThenWebhookReturns200Response")]
+        //Cell Movement
+        [TestCase("MoveCell.JSON", "PermitWithSameKey", TestName = "WhenICallTheWebhookWithMoveCellScenario_ThenWebhookReturns200Response")]
+        [TestCase("MultipleCellsInSingleUnit.JSON", "PermitWithSameKey", TestName = "WhenICallTheWebhookWithMultipleCellsInSingleUnitScenario_ThenWebhookReturns200Response")]
 
-        //Move Cell
-        [TestCase("ID14_moveOneCell.JSON", "Y", TestName = "WhenICallTheWebhookWithSimpleMoveCellScenario_ThenWebhookReturns200Response")]
-        [TestCase("ID15_oneNewCellAndOneMoveOneCell.JSON", "N", "SamePermitKey", TestName = "WhenICallTheWebhookWithOneNewCellAndOneMoveOneCellScenario_ThenWebhookReturns200Response")]
+        //Mixed Scenarios
+        [TestCase("NewCell_AND_CancelCellWithNewCellReplacement.JSON", "PermitWithSameKey", TestName = "WhenICallTheWebhookWithNewCell_AND_CancelCellWithNewCellReplacementScenario_ThenWebhookReturns200Response")]
+        [TestCase("NewCell_AND_MoveCell.JSON", "PermitWithSameKey", TestName = "WhenICallTheWebhookWithNewCell_AND_MoveCellScenario_ThenWebhookReturns200Response")]
+        [TestCase("NewCell_AND_UoSMetadataChange.JSON", "PermitWithDifferentKey", TestName = "WhenICallTheWebhookWithNewCell_AND_UoSMetadataChangeScenario_ThenWebhookReturns200Response")]
+        [TestCase("NewCell_AND_UpdateCell_AND_CellMetadataChange.JSON", "PermitWithSameKey", TestName = "WhenICallTheWebhookWithNewCell_AND_UpdateCell_AND_CellMetadataChangeScenario_ThenWebhookReturns200Response")]
+        [TestCase("CellMetadataChange_AND_SuspendCell.JSON", "PermitWithSameKey", TestName = "WhenICallTheWebhookWithCellMetadataChange_AND_SuspendCellScenario_ThenWebhookReturns200Response")]
+        [TestCase("CellMetadataChangeWithNewCell.JSON", "PermitWithDifferentKey", TestName = "WhenICallTheWebhookWithCellMetadataChangeWithNewCellScenario_ThenWebhookReturns200Response")]
+        [TestCase("MoveCell_AND_SuspendCell.JSON", "PermitWithSameKey", TestName = "WhenICallTheWebhookWithMoveCell_AND_SuspendCellScenario_ThenWebhookReturns200Response")]
 
-        //Mixed
-        [TestCase("ID16_newCell_updateCell_metadataChange.JSON", "N", "SamePermitKey", TestName = "WhenICallTheWebhookWithMixScenarioHavingNewCellAndUpdateCellAndMetadataChange_ThenWebhookReturns200Response")]
-        [TestCase("ID17_newCell_and_CancelReplace.JSON", "N", "SamePermitKey", TestName = "WhenICallTheWebhookWithMixScenarioHavingOneNewCellAndOneCancel&ReplaceCell_ThenWebhookReturns200Response")]
-        [TestCase("ID18_CancelReplace_UpdateCell.JSON", "N", "SamePermitKey", TestName = "WhenICallTheWebhookWithMixScenarioHavingCancel&Replace_UpdateCell_ThenWebhookReturns200Response")]
-        [TestCase("ID19_CR_metadata_move.JSON", "N", "SamePermitKey", TestName = "WhenICallTheWebhookWithMixScenarioHavingCancel&ReplaceAndMetadataChangeAndMoveCell_ThenWebhookReturns200Response")]
+        //Suspend and Withdraw Scenarios
+        [TestCase("SuspendCell.JSON", "PermitWithSameKey", TestName = "WhenICallTheWebhookWithSuspendCellScenario_ThenWebhookReturns200Response")]
+        [TestCase("SuspendCell_AND_WithdrawCell.JSON", "PermitWithDifferentKey", TestName = "WhenICallTheWebhookWithSuspendCell_AND_WithdrawCellScenario_ThenWebhookReturns200Response")]
+        [TestCase("WithdrawCell.JSON", "PermitWithSameKey", TestName = "WhenICallTheWebhookWithWithdrawCellScenario_ThenWebhookReturns200Response")]
 
-        //New Edition
-        [TestCase("ID20_newEditionAdditionalCoverageV_01.JSON", "N", "SamePermitKey", TestName = "WhenICallTheWebhookWithNewEditionAdditionalCoverageV01PayloadFile_ThenWebhookReturns200Response")]
+        //Update Scenarios
+        [TestCase("UpdateCell.JSON", "PermitWithSameKey", TestName = "WhenICallTheWebhookWithUpdateCellScenario_ThenWebhookReturns200Response")]
+        [TestCase("UpdateCellsWithDifferentStatusName.JSON", "PermitWithDifferentKey", TestName = "WhenICallTheWebhookWithUpdateCellsWithDifferentStatusNameScenario_ThenWebhookReturns200Response")]
+        [TestCase("UpdateCellWithNewEdition.JSON", "PermitWithSameKey", TestName = "WhenICallTheWebhookWithUpdateCellWithNewEditionScenario_ThenWebhookReturns200Response")]
 
-        //V0.3S
-        [TestCase("ID21_cancelAndReplaceV_03.JSON", "N", "SamePermitKey", TestName = "WhenICallTheWebhookWithCancelAndReplaceV03PayloadFile_ThenWebhookReturns200Response")]
-        [TestCase("ID22_Cell_Moves_Unit_and_New_CellV_03.JSON", "N", "SamePermitKey", TestName = "WhenICallTheWebhookWithCellMoveAndNewCellV03PayloadFile_ThenWebhookReturns200Response")]
-        [TestCase("ID23_Cell_MoveV_03.JSON", "N", TestName = "WhenICallTheWebhookWithCellMoveV03PayloadFile_ThenWebhookReturns200Response")]
-        [TestCase("ID24_Metadata_ChangeV_03.JSON", "N", TestName = "WhenICallTheWebhookWithMetadataChangeV03PayloadFile_ThenWebhookReturns200Response")]
-        [TestCase("ID25_Mixed_scenario1V_03.JSON", "N", "SamePermitKey", TestName = "WhenICallTheWebhookWithMixedScenario1V03PayloadFile_ThenWebhookReturns200Response")]
-        [TestCase("ID26_New_CellV_03.JSON", "N", "SamePermitKey", TestName = "WhenICallTheWebhookWithNewCellV03PayloadFile_ThenWebhookReturns200Response")]
-
-        //Supplier Defined Releasability Set v0.1
-        [TestCase("ID27_supplier_Defined_ReleasabilitySet_V_01.JSON", "Y", "SamePermitKe", TestName = "WhenICallTheWebhookWithSupplierDefinedReleasabilitySetV01_ThenWebhookReturns200Response")]
-
-        //Suspended & Withdrawn
-        [TestCase("ID28_simpleSuspendedScenario.JSON", "N", "DifferentPermitKey", TestName = "WhenICallTheWebhookWithSimpleSuspendedScenario_ThenWebhookReturns200Response")]
-        [TestCase("ID29_simpleWithdrawnScenario.JSON", "N", TestName = "WhenICallTheWebhookWithSimpleWithdrawnScenario_ThenWebhookReturns200Response")]
-        [TestCase("ID30_Suspend_and_WithdrawV01.JSON", "N", "SamePermitKey", TestName = "WhenICallTheWebhookWithSuspendedAndWithdrawnScenario_ThenWebhookReturns200Response")]
-        [TestCase("ID31_metadataAndSuspended.JSON", "N", "SamePermitKey", TestName = "WhenICallTheWebhookWithMetadataAndSuspendedMixScenario_ThenWebhookReturns200Response")]
-        [TestCase("ID32_moveAndSuspended.JSON", "N", "SamePermitKey", TestName = "WhenICallTheWebhookWithMoveDataAndSuspendedMixScenario_ThenWebhookReturns200Response")]
-
-        //Rule change unitType & addProducts
-        [TestCase("ID33_NewCell_With2UoS_But_only1_having_addProduct.JSON", "N", "SamePermitKey", TestName = "WhenICallTheWebhookWithNewCellScenarioWithMultipleUoSHavingUnitOfSalesTypeUnitButOnly1HavingValueInAddProducts_ThenWebhookReturns200Response")]
-        [TestCase("ID34_Cancel&Replace_With_NewCells_having_2UoS_With_addProductValue.JSON", "N", "SamePermitKey", TestName = "WhenICallTheWebhookWithCancel&ReplaceScenarioHavingMultipleUnitOfSalesTypeUnitAndValueInAddProducts_ThenWebhookReturns200Response")]
-        [TestCase("ID35_Cancel&Replace_With_CancelCell_having_2UoS_but_onlyOneAsTypeUnit.JSON", "N", "SamePermitKey", TestName = "WhenICallTheWebhookWithCancel&ReplaceWithCancelledCellHaving2UoSButOnly1IsOfTypeUnit_ThenWebhookReturns200Response")]
-        [TestCase("ID36_MoveAndSuspended_With_2UoS_But1_Having_addProductsValue.JSON", "N", "SamePermitKey", TestName = "WhenICallTheWebhookWithMoveAndSuspendedWith2UoSButOnly1HavingValueInAddProduct_ThenWebhookReturns200Response")]
-
-        // Rule change for create avcs unit of sale (having multiple products in addProducts)
-        [TestCase("ID37_CreateUoSHavingMultipleItemsInAddProducts.JSON", "N", "SamePermitKey", TestName = "WhenICallTheWebhookWithMoveAndNewCellScenarioWhereUoSHasMultipleValuesInAddProducts_ThenWebhookReturns200Response")]
-
-        [TestCase("ID38_3AdditionalCoverageCell.JSON", "N", "SamePermitKey", TestName = "WhenICallTheWebhookWithThreeAdditionalCoverageCellScenario_ThenWebhookReturns200Response")]
-        [TestCase("ID39_1AdditionalCoverageWith1CancelledCell.JSON", "N", "SamePermitKey", TestName = "WhenICallTheWebhookWithOneAdditionalCoverageWithCancelledCellScenario_ThenWebhookReturns200Response")]
-        [TestCase("ID40_1AdditionalCoverageWithReplacedCellAnd1CancelledCell.JSON", "N", "SamePermitKey", TestName = "WhenICallTheWebhookWithOneAdditionalCoverageWithReplacedAndOneCancelledCellScenario_ThenWebhookReturns200Response")]
-        public async Task WhenValidEventInNewEncContentPublishedEventReceivedWithValidToken_ThenWebhookReturns200OkResponse1(string payloadJsonFileName, string correctionTag, string permitState = "permitString")
+        public async Task WhenValidEventInNewEncContentPublishedEventReceivedWithValidToken_ThenWebhookReturns200OkResponse(string payloadJsonFileName, string permitState)
         {
             Console.WriteLine("Scenario:" + payloadJsonFileName + "\n");
             string filePath = Path.Combine(_projectDir, Config.TestConfig.PayloadFolder, payloadJsonFileName);
             string generatedXmlFolder = Path.Combine(_projectDir, Config.TestConfig.GeneratedXmlFolder);
-            RestResponse response = await WebhookEndpoint.PostWebhookResponseAsyncForXml(filePath, generatedXmlFolder, await _authToken.GetAzureADToken(false), correctionTag, permitState);
-            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+            RestResponse response = await WebhookEndpoint.PostWebhookResponseAsyncForXml(filePath, generatedXmlFolder, await _authToken.GetAzureADToken(false), permitState );
+            switch (payloadJsonFileName)
+            {
+                case "UnauthorizedResponseFromSap.JSON":
+                case "InternalServerErrorFromSap.JSON":
+                    response.StatusCode.Should().Be(System.Net.HttpStatusCode.InternalServerError);
+                    break;
+
+                default:
+                    response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+                    break;
+            }
+        }
+
+        [Test]
+        //Mandatory attribute validation scenarios
+        [TestCase("products.productName", 1, TestName = "WhenProductNameIsEmptyOrNull_ThenWebhookShouldReturn500ResponseCode")]
+        [TestCase("unitsOfSale.unitName", 1, TestName = "WhenUnitNameIsEmptyOrNull_ThenWebhookShouldReturn500ResponseCode")]
+        [TestCase("products.providerCode", 1, TestName = "WhenProductProviderCodeIsEmptyOrNull_ThenWebhookShouldReturn500ResponseCode")]
+        [TestCase("unitsOfSale.providerCode", 1, TestName = "WhenUnitProviderCodeIsEmptyOrNull_ThenWebhookShouldReturn500ResponseCode")]
+        [TestCase("products.size", 1, TestName = "WhenProductSizeIsEmptyOrNull_ThenWebhookShouldReturn500ResponseCode")]
+        [TestCase("unitsOfSale.unitSize", 1, TestName = "WhenUnitSizeIsEmptyOrNull_ThenWebhookShouldReturn500ResponseCode")]
+        [TestCase("products.title", 1, TestName = "WhenProductTitleIsEmptyOrNull_ThenWebhookShouldReturn500ResponseCode")]
+        [TestCase("unitsOfSale.title", 1, TestName = "WhenUnitTitleIsEmptyOrNull_ThenWebhookShouldReturn500ResponseCode")]
+        [TestCase("products.editionNumber", 1, TestName = "WhenProductEditionNumberIsEmptyOrNull_ThenWebhookShouldReturn500ResponseCode")]
+        [TestCase("products.updateNumber", 1, TestName = "WhenProductUpdateNumberIsEmptyOrNull_ThenWebhookShouldReturn500ResponseCode")]
+        [TestCase("unitsOfSale.unitType", 1, TestName = "WhenUnitTypeIsEmptyOrNull_ThenWebhookShouldReturn500ResponseCode")]
+        [TestCase("year", 0, TestName = "WhenYearIsEmptyOrNull_ThenWebhookShouldReturn500ResponseCode")]
+        [TestCase("week", 0, TestName = "WhenWeekIsEmptyOrNull_ThenWebhookShouldReturn500ResponseCode")]
+        [TestCase("currentWeekAlphaCorrection", 0, TestName = "WhenAlphaCorrectionFlagIsEmptyOrNull_ThenWebhookShouldReturn500ResponseCode")]
+        [TestCase("products.permit", 1, TestName = "WhenProductPermitIsEmptyOrNull_ThenWebhookShouldReturn500ResponseCode")]
+        public async Task WhenMandatoryAttributeIsEmptyOrNullInPayload_ThenWebhookReturnsInternalServerErrorResponse(string attributeName, int index)
+        {
+            string filePath = Path.Combine(_projectDir, Config.TestConfig.PayloadFolder, "MandatoryAttributeValidation.JSON");
+            RestResponse response = await WebhookEndpoint.PostWebhookResponseForMandatoryAttributeValidation(filePath, await _authToken.GetAzureADToken(false), attributeName, index, "Remove");
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.InternalServerError);
+
+            response = await WebhookEndpoint.PostWebhookResponseForMandatoryAttributeValidation(filePath, await _authToken.GetAzureADToken(false), attributeName, index, "EmptyOrNull");
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.InternalServerError);
         }
     }
 }
