d66773173f1e441d7b4daf377dfb7d0a5cd84b63
Prasad Rasal <Prasad14878@Mastek.Com>
2024-10-04 16:06:13 +0530

    Added Models for s100 event AB#180094

---
+++ src/UKHO.ERPFacade.Common/Models/S100Event/S100CompositionChanges.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace UKHO.ERPFacade.Common.Models.S100Event
+{
+    public class S100CompositionChanges
+    {
+        [JsonProperty("addProducts")]
+        public List<string> AddProducts { get; set; }
+        [JsonProperty("removeProducts")]
+        public List<string> RemoveProducts { get; set; }
+    }
+}
---
+++ src/UKHO.ERPFacade.Common/Models/S100Event/S100UnitsOfSale.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace UKHO.ERPFacade.Common.Models.S100Event
+{
+    public class S100UnitsOfSale
+    {
+        [JsonProperty("unitName")]
+        public string UnitName { get; set; }
+        [JsonProperty("title")]
+        public string Title { get; set; }
+        [JsonProperty("unitSize")]
+        public string UnitSize { get; set; }
+        [JsonProperty("status")]
+        public string Status { get; set; }
+        [JsonProperty("isNewUnitOfSale")]
+        public bool IsNewUnitOfSale { get; set; }
+        [JsonProperty("boundingBox")]
+        public S100BoundingBox BoundingBox { get; set; }
+        [JsonProperty("compositionChanges")]
+        public S100CompositionChanges CompositionChanges { get; set; }
+    }
+}
