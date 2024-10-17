f0ff8174d643b081576d73cb463bf9fccf61b54b
Pooja More <135319691+pooja15131@users.noreply.github.com>
2024-10-17 21:22:43 +0530

    181622 exclude aio cells (#217)
    * 181622 - development & UTs to exclude aio cells from erp facade newenccontentpublishedeventreceived s57 processing.
    
    * 181622 - AIO Test data file renamed And UTs added with new test data files for stryker coverage improvment.
    
    * 181622-Added FTs for AIO scenarios- AB#181622
    
    * 181622-Added constant for AIO-AB#181622
    
    * 181623-code refactored for stryker AB#181623
    
    * 181622 - UT added to improve stryker coverage
    
    * 181622-removed unused namespace AB#181622
    
    * 181623-code refactored for stryker improvement AB#181623
    
    * 181622-code refactored for aio cell AB#181622
    
    * 181622-removed scenario for mixed cell payload AB#181622
    
    * 181622-log updated AB#181622
    
    * 181622-removed sucess status code from log AB#181622
    
    ---------
    
    Co-authored-by: Pooja More <pooja15131@mastek.com>
    Co-authored-by: rushikeshkhopatkar <rushike12794@mastek.com>
    Co-authored-by: Pravin Varade <Pravin.Varade2@mastek.com>
    Co-authored-by: Vishal Dukare <Vishal14583@Mastek.Com>
    Co-authored-by: rushdynajath <rushdy.najath@ukho.gov.uk>

---
+++ src/UKHO.ERPFacade.API/Services/S57Service.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using UKHO.ERPFacade.API.Helpers;
+using UKHO.ERPFacade.Common.Configuration;
+using UKHO.ERPFacade.Common.Constants;
+using UKHO.ERPFacade.Common.Exceptions;
+using UKHO.ERPFacade.Common.HttpClients;
+using UKHO.ERPFacade.Common.IO;
+using UKHO.ERPFacade.Common.IO.Azure;
+using UKHO.ERPFacade.Common.Logging;
+using UKHO.ERPFacade.Common.Models;
+using UKHO.ERPFacade.Common.Models.TableEntities;
+
+namespace UKHO.ERPFacade.API.Services
+{
+    public class S57Service : IS57Service
+    {
+        private readonly ILogger<S57Service> _logger;
+        private readonly IAzureTableReaderWriter _azureTableReaderWriter;
+        private readonly IAzureBlobEventWriter _azureBlobEventWriter;
+        private readonly IAzureQueueHelper _azureQueueHelper;
+        private readonly ISapClient _sapClient;
+        private readonly IEncContentSapMessageBuilder _encContentSapMessageBuilder;
+        private readonly IOptions<SapConfiguration> _sapConfig;
+        private readonly IOptions<AioConfiguration> _aioConfig;
+        private List<string> _aioCells = [];
+
+        public S57Service(ILogger<S57Service> logger,
+                                 IAzureTableReaderWriter azureTableReaderWriter,
+                                 IAzureBlobEventWriter azureBlobEventWriter,
+                                 IAzureQueueHelper azureQueueHelper,
+                                 ISapClient sapClient,
+                                 IEncContentSapMessageBuilder encContentSapMessageBuilder,
+                                 IOptions<SapConfiguration> sapConfig,
+                                 IOptions<AioConfiguration> aioConfig)
+        {
+            _logger = logger;
+            _azureTableReaderWriter = azureTableReaderWriter;
+            _azureBlobEventWriter = azureBlobEventWriter;
+            _azureQueueHelper = azureQueueHelper;
+            _sapClient = sapClient;
+            _encContentSapMessageBuilder = encContentSapMessageBuilder;
+            _sapConfig = sapConfig ?? throw new ArgumentNullException(nameof(sapConfig));
+            _aioConfig = aioConfig;
+
+            _aioCells = !string.IsNullOrEmpty(_aioConfig.Value.AioCells) ? new(_aioConfig.Value.AioCells.Split(',').Select(s => s.Trim())) :
+                throw new ERPFacadeException(EventIds.AioConfigurationNotFoundException.ToEventId(), "Aio cell configuration not found.");
+        }
+
+        public async Task ProcessS57Event(JObject encEventJson)
+        {
+            var eventData = JsonConvert.DeserializeObject<EncEventPayload>(encEventJson.ToString());
+
+            if (IsAioCell(eventData.Data.Products.Select(x => x.ProductName).ToList()))
+            {
+                _logger.LogInformation(EventIds.NoProcessingOfNewEncContentPublishedEventForAioCells.ToEventId(), "The enccontentpublished event will not be processed for Aio cells.");
+                return;
+            }
+
+            EncEventEntity encEventEntity = new()
+            {
+                RowKey = Guid.NewGuid().ToString(),
+                PartitionKey = Guid.NewGuid().ToString(),
+                Timestamp = DateTime.UtcNow,
+                CorrelationId = eventData.Data.CorrelationId,
+                RequestDateTime = null
+            };
+
+            _logger.LogInformation(EventIds.AddingEntryForEncContentPublishedEventInAzureTable.ToEventId(), "Adding/Updating entry for enccontentpublished event in azure table.");
+            await _azureTableReaderWriter.UpsertEntity(eventData.Data.CorrelationId, Constants.S57EventTableName, encEventEntity);
+
+            _logger.LogInformation(EventIds.UploadEncContentPublishedEventInAzureBlobStarted.ToEventId(), "Uploading enccontentpublished event payload in blob storage.");
+            await _azureBlobEventWriter.UploadEvent(encEventJson.ToString(), Constants.S57EventContainerName, eventData.Data.CorrelationId + '/' + Constants.S57EncEventFileName);
+            _logger.LogInformation(EventIds.UploadEncContentPublishedEventInAzureBlobCompleted.ToEventId(), "The enccontentpublished event payload is uploaded in blob storage successfully.");
+
+            var sapPayload = _encContentSapMessageBuilder.BuildSapMessageXml(JsonConvert.DeserializeObject<EncEventPayload>(encEventJson.ToString()));
+
+            _logger.LogInformation(EventIds.UploadSapXmlPayloadInAzureBlobStarted.ToEventId(), "Uploading the SAP XML payload in blob storage.");
+            await _azureBlobEventWriter.UploadEvent(sapPayload.ToIndentedString(), Constants.S57EventContainerName, eventData.Data.CorrelationId + '/' + Constants.SapXmlPayloadFileName);
+            _logger.LogInformation(EventIds.UploadSapXmlPayloadInAzureBlobCompleted.ToEventId(), "SAP XML payload is uploaded in blob storage successfully.");
+
+            var response = await _sapClient.PostEventData(sapPayload, _sapConfig.Value.SapEndpointForEncEvent, _sapConfig.Value.SapServiceOperationForEncEvent, _sapConfig.Value.SapUsernameForEncEvent, _sapConfig.Value.SapPasswordForEncEvent);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ERPFacadeException(EventIds.RequestToSapFailed.ToEventId(), $"An error occurred while sending a request to SAP. | {response.StatusCode}");
+            }
+            _logger.LogInformation(EventIds.EncUpdateSentToSap.ToEventId(), "ENC update has been sent to SAP successfully");
+
+            await _azureTableReaderWriter.UpdateEntity(eventData.Data.CorrelationId, Constants.S57EventTableName, new[] { new KeyValuePair<string, DateTime>("RequestDateTime", DateTime.UtcNow) });
+        }
+
+        private bool IsAioCell(IEnumerable<string> products)
+        {
+            return products.Any(_aioCells.Contains);
+        }
+    }
+}
