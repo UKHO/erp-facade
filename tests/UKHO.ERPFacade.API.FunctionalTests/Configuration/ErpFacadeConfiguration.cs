63c945c80ce6861e8f0b772861e24451423eab39
Vishal Dukare <Vishal14583@Mastek.Com>
2024-11-08 17:13:04 +0000

    189099-functional test project structure uplifted AB#189099

---
+++ tests/UKHO.ERPFacade.API.FunctionalTests/Configuration/ErpFacadeConfiguration.cs
@@ -0,0 +1,28 @@
+﻿namespace UKHO.ERPFacade.API.FunctionalTests.Configuration
+{
+    public class ErpFacadeConfiguration
+    {
+        public string BaseUrl { get; set; }
+        public string WebhookEndpointUrl { get; set; }
+        public string LicenceUpdatedRequestEndPoint { get; set; }
+        public string RoSWebhookRequestEndPoint { get; set; }
+        public PermitWithSameKey PermitWithSameKey { get; set; }
+        public PermitWithDifferentKey PermitWithDifferentKey { get; set; }
+        public string[] RosLicenceUpdateXmlList { get; set; }
+        public string[] RoSLicenceUpdatedProdXmlList { get; set; }
+    }
+
+    public class PermitWithSameKey
+    {
+        public string Permit { get; set; }
+        public string ActiveKey { get; set; }
+        public string NextKey { get; set; }
+    }
+
+    public class PermitWithDifferentKey
+    {
+        public string Permit { get; set; }
+        public string ActiveKey { get; set; }
+        public string NextKey { get; set; }
+    }
+}
