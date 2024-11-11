a14e66e520644d5457965dddd0467edf7d4eda18
Vishal Dukare <Vishal14583@Mastek.Com>
2024-11-11 12:37:52 +0000

    189099- section added for FTs constants AB#189099

--- src/UKHO.ERPFacade.Common/Constants/EventPayloadFiles.cs
+++ src/UKHO.ERPFacade.Common/Constants/EventPayloadFiles.cs
@@ -10,15 +10,23 @@ namespace UKHO.ERPFacade.Common.Constants
         public const string SapXmlPayloadFileName = "SapXmlPayload.xml";
         public const string S100DataEventFileName = "S100DataPublishingEvent.json";
         public const string RecordOfSaleEventFileExtension = ".json";
-        public const string S100PayloadFolder = "S100PayloadTestData";
-        public const string S100ExpectedXmlFiles = "S100ExpectedXmlFiles";
-        public const string ErpFacadeExpectedXmlFolder = "ERPFacadeExpectedXmlFiles";
-        public const string S57ExpectedXmlFolder = "S57ExpectedXmlFiles";
-        public const string RosPayloadTestDataFolder = "RoSPayloadTestData";
-        public const string LicenceUpdatedPayloadTestData = "LicenceUpdatedPayloadTestData";
+
+        /// <summary>
+        /// Constants for functional test project files and folder names
+        /// </summary>
+        ///
         public const string PayloadFolder = "ERPFacadePayloadTestData";
+        public const string LicenceUpdatedPayloadTestData = "LicenceUpdatedPayloadTestData";
+        public const string RosPayloadTestDataFolder = "RoSPayloadTestData";
         public const string S57PayloadFolder = "S57PayloadTestData";
+        public const string S100PayloadFolder = "S100PayloadTestData";
+
         public const string WebhookPayloadFileName = "WebhookPayload.JSON";
+
         public const string GeneratedXmlFolder = "ERPFacadeGeneratedXmlFiles";
+
+        public const string ErpFacadeExpectedXmlFolder = "ERPFacadeExpectedXmlFiles";
+        public const string S100ExpectedXmlFiles = "S100ExpectedXmlFiles";
+        public const string S57ExpectedXmlFolder = "S57ExpectedXmlFiles";
     }
 }
