4918071945308ee8d9c7f7c60b191f79ce5a2a63
rushikeshkhopatkar <rushikesh.khopatkar@mastek.com>
2024-11-12 18:34:21 +0530

    Feature/180588 replace existing mock with wiremock (#250)
    * 180588-existing mock service replaced with wiremock for SAP and EES AB#180588
    
    * AB #180588 - Added SAP callback action
    
    * AB #180588 - Added SapCallbackConfiguration.SharedApiKey
    
    * AB#180588-added baseurl for callback endpoint
    
    * Added missing file
    
    * AB#180588 - updated namespace
    
    * AB#180588 - Removed unused entries from csproj file
    
    * AB#180588 - minor spelling corrections & removed unused packages
    
    ---------
    
    Co-authored-by: Vishal Dukare <Vishal14583@Mastek.Com>
    Co-authored-by: Rushikesh Khopatkar <rushike12794@mastek.com>
    Co-authored-by: Singaraiah Kokkera <singa1501799@mastek.com>

---
+++ src/UKHO.SAP.MockAPIService/StubSetup/StubFactory.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Options;
+using UKHO.SAP.MockAPIService.Configuration;
+using UKHO.SAP.MockAPIService.Stubs;
+
+namespace UKHO.SAP.MockAPIService.StubSetup
+{
+    public class StubFactory
+    {
+        private readonly S57EncEventConfiguration _encEventConfiguration;
+        private readonly RecordOfSaleEventConfiguration _recordOfSaleEventConfiguration;
+        private readonly S100DataEventConfiguration _s100DataEventConfiguration;
+        private readonly EesConfiguration _eesConfiguration;
+        private readonly SapCallbackConfiguration _sapCallbackConfiguration;
+        private readonly ErpFacadeConfiguration _erpFacadeConfiguration;
+
+        public StubFactory(IOptions<S57EncEventConfiguration> encEventConfiguration,
+                           IOptions<RecordOfSaleEventConfiguration> recordOfSaleEventConfiguration,
+                           IOptions<S100DataEventConfiguration> s100DataEventConfiguration,
+                           IOptions<EesConfiguration> eesConfiguration,
+                           IOptions<SapCallbackConfiguration> sapCallbackConfiguration,
+                           IOptions<ErpFacadeConfiguration> erpFacadeConfiguration)
+        {
+            _encEventConfiguration = encEventConfiguration?.Value ?? throw new ArgumentNullException(nameof(encEventConfiguration));
+            _recordOfSaleEventConfiguration = recordOfSaleEventConfiguration.Value ?? throw new ArgumentNullException(nameof(recordOfSaleEventConfiguration));
+            _s100DataEventConfiguration = s100DataEventConfiguration.Value ?? throw new ArgumentNullException(nameof(s100DataEventConfiguration));
+            _eesConfiguration = eesConfiguration.Value ?? throw new ArgumentNullException(nameof(eesConfiguration));
+            _sapCallbackConfiguration = sapCallbackConfiguration.Value ?? throw new ArgumentNullException(nameof(sapCallbackConfiguration));
+            _erpFacadeConfiguration = erpFacadeConfiguration.Value ?? throw new ArgumentNullException(nameof(erpFacadeConfiguration));
+        }
+
+        public IStub CreateSapStub()
+        {
+            return new SapStub(_encEventConfiguration, _recordOfSaleEventConfiguration, _s100DataEventConfiguration, _sapCallbackConfiguration, _erpFacadeConfiguration);
+        }
+
+        public IStub CreateEesStub()
+        {
+            return new EesStub(_eesConfiguration);
+        }
+    }
+}
