dfec3fea88bb3a55d600daf740c8b2278d064422
Pooja More <pooja15131@mastek.com>
2024-11-19 19:27:04 +0530

    180087 - Implementation to consume EES endpoint to publish unitofsaleupdated event AB#180087 AB#182323

--- src/UKHO.ERPFacade.Common/HttpClients/IEESClient.cs
+++ src/UKHO.ERPFacade.Common/HttpClients/IEESClient.cs
@@ -1,7 +1,11 @@
-﻿namespace UKHO.ERPFacade.Common.HttpClients
+﻿using UKHO.ERPFacade.Common.Models;
+using UKHO.ERPFacade.Common.Models.CloudEvents;
+
+namespace UKHO.ERPFacade.Common.HttpClients
 {
     public interface IEESClient
     {
         Task<HttpResponseMessage> Get(string url);
+        Task<Result> PostAsync(BaseCloudEvent cloudEvent);
     }
 }
