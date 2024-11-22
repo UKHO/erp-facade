ff7131f6dce00b7391362834fb7e2f1a616f45c8
Vishal Dukare <Vishal14583@Mastek.Com>
2024-11-22 12:56:28 +0000

    185613-removed log

--- src/UKHO.ERPFacade.Common/Policies/RetryPolicyProvider.cs
+++ src/UKHO.ERPFacade.Common/Policies/RetryPolicyProvider.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using Microsoft.Extensions.Logging;
 using Polly;
+using Polly.Extensions.Http;
 
 namespace UKHO.ERPFacade.Common.Policies
 {
@@ -10,33 +11,12 @@ namespace UKHO.ERPFacade.Common.Policies
     {
         public static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy(ILogger logger, int retryCount, double sleepDuration)
         {
-            return Policy
-                .HandleResult<HttpResponseMessage>(r => r.StatusCode == HttpStatusCode.ServiceUnavailable)
-                .OrResult(r => r.StatusCode == HttpStatusCode.TooManyRequests)
-                .OrResult(r => r.StatusCode == HttpStatusCode.InternalServerError)
-                .WaitAndRetryAsync(retryCount, (retryAttempt) =>
+            return HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .WaitAndRetryAsync(retryCount, retryAttempt => TimeSpan.FromSeconds(sleepDuration),
+                onRetry: (response, timespan, retryAttempt, context) =>
                 {
-                    return TimeSpan.FromSeconds(Math.Pow(sleepDuration, (retryAttempt - 1)));
-                }, async (response, timespan, retryAttempt, context) =>
-                {
-                    var retryAfterHeader = response.Result.Headers.FirstOrDefault(h => h.Key.ToLowerInvariant() == "retry-after");
-                    var correlationId = response.Result.RequestMessage.Headers.FirstOrDefault(h => h.Key.ToLowerInvariant() == "x-correlation-id");
-                    int retryAfter = 0;
-                    if (response.Result.StatusCode == HttpStatusCode.TooManyRequests && retryAfterHeader.Value != null && retryAfterHeader.Value.Any())
-                    {
-                        retryAfter = int.Parse(retryAfterHeader.Value.First());
-                        await Task.Delay(TimeSpan.FromMilliseconds(retryAfter));
-                    }
-                    logger.LogInformation("Re-trying service request with uri {RequestUri} and delay {delay}ms and retry attempt {retry} with _X-Correlation-ID:{correlationId} as previous request was responded with {StatusCode}.",
-                     response.Result.RequestMessage.RequestUri, timespan.Add(TimeSpan.FromMilliseconds(retryAfter)).TotalMilliseconds, retryAttempt, correlationId.Value, response.Result.StatusCode);
                 });
-
-            //return HttpPolicyExtensions
-            //.HandleTransientHttpError()
-            //.WaitAndRetryAsync(retryCount, retryAttempt => TimeSpan.FromSeconds(sleepDuration),
-            //onRetry: (response, timespan, retryAttempt, context) =>
-            //{
-            //});
         }
     }
 }
