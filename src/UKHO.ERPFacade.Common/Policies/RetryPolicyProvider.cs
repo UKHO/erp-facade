95241dbaa84192334e1f1f47f519696b235bdcd4
Gaurav Mejari <gaurav15233@mastek.com>
2024-11-22 18:38:15 +0530

    AB#185613 Reverted changes for logger

--- src/UKHO.ERPFacade.Common/Policies/RetryPolicyProvider.cs
+++ src/UKHO.ERPFacade.Common/Policies/RetryPolicyProvider.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics.CodeAnalysis;
 using System.Net;
-using Azure;
 using Microsoft.Extensions.Logging;
 using Polly;
 using Polly.Extensions.Http;
@@ -18,14 +16,6 @@ namespace UKHO.ERPFacade.Common.Policies
                 .WaitAndRetryAsync(retryCount, retryAttempt => TimeSpan.FromSeconds(sleepDuration),
                 onRetry: (response, timespan, retryAttempt, context) =>
                 {
-                    if (response.Exception != null)
-                    {
-                        logger.LogError(response.Exception, $"Retry {retryAttempt} after {timespan.TotalSeconds} seconds due to exception.");
-                    }
-                    else if (response.Result != null)
-                    {
-                        logger.LogWarning($"Retry {retryAttempt} after {timespan.TotalSeconds} seconds due to response: {response.Result.ReasonPhrase}");
-                    }
                 });
         }
     }
