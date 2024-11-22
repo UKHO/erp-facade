3af8034150c40777a48192fdb1beb4c2ebf6fd13
Tukaram Pandhare <tukar1501502@mastek.com>
2024-11-22 15:12:52 +0530

    AB#185613 tried workaround fro snyk issue

--- src/UKHO.ERPFacade.Common/Policies/RetryPolicyProvider.cs
+++ src/UKHO.ERPFacade.Common/Policies/RetryPolicyProvider.cs
@@ -2,20 +2,20 @@
 using Microsoft.Extensions.Logging;
 using Polly;
 using Polly.Extensions.Http;
-using UKHO.ERPFacade.Common.Logging;
 
 namespace UKHO.ERPFacade.Common.Policies
 {
     [ExcludeFromCodeCoverage]
     public static class RetryPolicyProvider
     {
-        public static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy(ILogger _logger, string service, EventIds eventId, int retryCount, double sleepDuration)
+        public static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy(ILogger _logger, int retryCount, double sleepDuration)
         {
             return HttpPolicyExtensions
             .HandleTransientHttpError()
             .WaitAndRetryAsync(retryCount, retryAttempt => TimeSpan.FromSeconds(sleepDuration),
             onRetry: (response, timespan, retryAttempt, context) =>
             {
+                _logger.LogError("Failed to connect Enterprise Event Service");
             });
         }
     }
