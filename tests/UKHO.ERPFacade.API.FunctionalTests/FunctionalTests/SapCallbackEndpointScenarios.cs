03966bad2697f4081f572f77e088a15e6793f0b5
rushikeshkhopatkar <rushike12794@mastek.com>
2024-11-25 10:37:43 +0530

    AB#185613-removed hardcoded guid

--- tests/UKHO.ERPFacade.API.FunctionalTests/FunctionalTests/SapCallbackEndpointScenarios.cs
+++ tests/UKHO.ERPFacade.API.FunctionalTests/FunctionalTests/SapCallbackEndpointScenarios.cs
@@ -27,7 +27,8 @@ namespace UKHO.ERPFacade.API.FunctionalTests.FunctionalTests
         [Test]
         public async Task WhenSapCallbackEndpointReceivesInvalidSharedApiKey_ThenSapCallbackEndpointReturns401UnauthorizedResponse()
         {
-            string payload = $"{{\r\n\"correlationId\": \"165ce4a4-1d62-4f56-b359-59e178d771041\"\r\n}}";
+            string correlationId = Guid.NewGuid().ToString();
+            string payload = $"{{\r\n\"correlationId\": \"{correlationId}\"\r\n}}";
             var response = await _sapCallbackEndpoint.PostSapCallbackEndPointResponseAsync(payload, true);
             response.StatusCode.Should().Be(System.Net.HttpStatusCode.Unauthorized);
         }
