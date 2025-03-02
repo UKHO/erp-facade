00dfaa2ab8569e6a41b4ba8ba84dfb22ca8cd045
rushdynajath <rushdy.najath@ukho.gov.uk>
2025-03-02 12:43:02 +0000

    add build pipeline to adds mock

--- Deployment/outputs.tf
+++ Deployment/outputs.tf
@@ -6,6 +6,10 @@ output "mock_webapp_name" {
   value = local.env_name == "dev" ? local.mock_web_app_name : null
 }
 
+output "adds_mock_webapp_name" {
+  value = local.adds_mock_web_app_name
+}
+
 output "storage_connection_string" {
   value = module.storage.storage_connection_string
   sensitive = true
