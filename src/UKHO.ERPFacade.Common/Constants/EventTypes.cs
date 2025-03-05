fd91b67e00905f5dd06935bc4f41ed49e672df60
Mohammed Khan <mohamme15315@mastek.com>
2025-03-05 17:55:10 +0530

    Added code for S100 Create rules AB#205201

--- src/UKHO.ERPFacade.Common/Constants/EventTypes.cs
+++ src/UKHO.ERPFacade.Common/Constants/EventTypes.cs
@@ -6,7 +6,7 @@ namespace UKHO.ERPFacade.Common.Constants
     public static class EventTypes
     {
         public const string S57EventType = "uk.gov.ukho.encpublishing.enccontentpublished.v2.2";
-        public const string S100EventType = "uk.gov.ukho.encpublishing.s100datacontentpublished.v1";
-        public const string S100UnitOfSaleUpdatedEventType = "uk.gov.ukho.erp.s100UnitOfSaleUpdated.v1";
+        public const string S100EventType = "uk.gov.ukho.encpublishing.s100datacontentpublished.v1.1";
+        public const string S100UnitOfSaleUpdatedEventType = "uk.gov.ukho.erp.s100UnitOfSaleUpdated.v1.1";
     }
 }
