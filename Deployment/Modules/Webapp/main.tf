6ab1b79f6e1286b21357f7929aeb2e287d108b10
rushdynajath <rushdy.najath@ukho.gov.uk>
2025-03-10 11:29:53 +0000

    clean up

--- Deployment/Modules/Webapp/main.tf
+++ Deployment/Modules/Webapp/main.tf
@@ -94,37 +94,6 @@ resource "azurerm_windows_web_app" "mock_webapp_service" {
   https_only = true
 }
 
-# resource "azurerm_windows_web_app" "addsmock_webapp_service" {
-#   count               = var.env_name == "iat" ? 1 : 0
-#   name                = var.addsmock_webapp_name
-#   location            = var.location
-#   resource_group_name = var.resource_group_name
-#   service_plan_id     = azurerm_service_plan.app_service_plan.id
-#   tags                = var.tags
-#   public_network_access_enabled      = false
-
-#   site_config {
-#     application_stack {    
-#       current_stack  = "dotnet"
-#       dotnet_version = "v8.0"
-#     }
-#     always_on  = true
-#     ftps_state = "Disabled"
-#   }
-     
-#   app_settings = var.mock_app_settings
-
-#   identity {
-#     type = "SystemAssigned"
-#   }
-
-#   lifecycle {
-#     ignore_changes = [ virtual_network_subnet_id ]
-#   }
-
-#   https_only = true
-# }
-
 resource "azurerm_app_service_virtual_network_swift_connection" "webapp_vnet_integration" {
   app_service_id = azurerm_windows_web_app.webapp_service.id
   subnet_id      = var.subnet_id
@@ -135,12 +104,6 @@ resource "azurerm_app_service_virtual_network_swift_connection" "mock_webapp_vne
   app_service_id = azurerm_windows_web_app.mock_webapp_service[0].id
   subnet_id      = var.subnet_id
 }
-
-# resource "azurerm_app_service_virtual_network_swift_connection" "addsmock_webapp_vnet_integration" {
-#   count               = var.env_name == "iat" ? 1 : 0
-#   app_service_id = azurerm_windows_web_app.addsmock_webapp_service[0].id
-#   subnet_id      = var.subnet_id
-# }
 
 resource "azurerm_app_service_slot_virtual_network_swift_connection" "slot_vnet_integration" {
   app_service_id = azurerm_windows_web_app.webapp_service.id
