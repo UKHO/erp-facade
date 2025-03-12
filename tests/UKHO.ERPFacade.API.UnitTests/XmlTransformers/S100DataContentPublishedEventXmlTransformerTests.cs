c539ac199bcbb03db616925e3c15b46b309da334
Mohammed Khan <mohamme15315@mastek.com>
2025-03-12 17:25:57 +0530

    Fixed minor review comment AB#205201

--- tests/UKHO.ERPFacade.API.UnitTests/XmlTransformers/S100DataContentPublishedEventXmlTransformerTests.cs
+++ tests/UKHO.ERPFacade.API.UnitTests/XmlTransformers/S100DataContentPublishedEventXmlTransformerTests.cs
@@ -137,9 +137,8 @@ namespace UKHO.ERPFacade.API.UnitTests.XmlTransformers
             XElement xElement = XElement.Parse(result.OuterXml);
             var itemList = xElement.Descendants("item").ToList();
             var s100WorldFolio = itemList.FirstOrDefault(item => item.Descendants("PRODUCTNAME").FirstOrDefault()?.Value == "s100WorldFolio");
-            Assert.That(itemList.Count > 0, Is.True);
+            Assert.That(itemList.Count, Is.EqualTo(3));
             Assert.That(s100WorldFolio == null, Is.True);
-
 
             A.CallTo(() => _fakeXmlOperations.AppendChildNode(A<XmlElement>.Ignored, A<XmlDocument>.Ignored, A<string>.Ignored, A<string>.Ignored)).MustHaveHappened(9, Times.Exactly);
 
