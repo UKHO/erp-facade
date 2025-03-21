efc066710a116fa3295b3ccfb43d87abbb4520dd
HugoBurgess <hugo.burgess@ukho.gov.uk>
2025-03-21 17:02:49 +0000

    Remove connection string altogether as env variable

--- Deployment/main.tf
+++ Deployment/main.tf
@@ -36,7 +36,6 @@ module "webapp_service" {
     "EventHubLoggingConfiguration:Environment"                 = local.env_name
     "EventHubLoggingConfiguration:MinimumLoggingLevel"         = "Warning"
     "EventHubLoggingConfiguration:UkhoMinimumLoggingLevel"     = "Information"
-    "APPLICATIONINSIGHTS_CONNECTION_STRING"                    = module.app_insights.connection_string
     "ASPNETCORE_ENVIRONMENT"                                   = local.env_name
     "WEBSITE_RUN_FROM_PACKAGE"                                 = "1"
     "WEBSITE_ENABLE_SYNC_UPDATE_SITE"                          = "true"
