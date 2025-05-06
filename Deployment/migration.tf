cde1f923095e7481161c6cbba934462584d34a47
rushdynajath <rushdy.najath@ukho.gov.uk>
2025-05-06 14:20:38 +0100

    clean up

--- Deployment/migration.tf
+++ Deployment/migration.tf
@@ -6,6 +6,6 @@ locals {
 
 import { 
     for_each = local.stubs
-    to = module.private_endpoint_link.azurerm_private_endpoint.main
+    to = module.private_endpoint_link.azurerm_private_endpoint.main[each.key]
     id = "/subscriptions/a3b2d2c4-28e8-4e1c-98c3-9804de5561b4/resourceGroups/erpfacade-vne-rg/providers/Microsoft.Network/privateEndpoints/m-erpvne2sap-vne-pe"
 }
