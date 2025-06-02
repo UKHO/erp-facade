64a5323fa02b24860e65c71055e56fd80cd5da5a
rushdynajath <rushdy.najath@ukho.gov.uk>
2025-06-02 14:38:44 +0100

    adds Mocks Override

---
+++ mock/UKHO.ADDS.Mocks.ERP/Override/Mocks/sap/GetZAddsMatInfo.cs
@@ -0,0 +1,32 @@
+﻿using UKHO.ADDS.Mocks.Markdown;
+using UKHO.ADDS.Mocks.States;
+
+namespace UKHO.ADDS.Mocks.ERP.Override.Mocks.sap    
+{
+    public class GetZAddsMatInfo : ServiceEndpointMock
+    {
+        public override void RegisterSingleEndpoint(IEndpointMock endpoint) =>
+            endpoint.MapPost("/z_adds_mat_info.asmx", (HttpRequest request) =>
+            {
+                var state = GetState(request);
+
+                switch (state)
+                {
+                    case WellKnownState.Default:
+                        // ADDS Mock will have the 'default' state unless we have told it otherwise
+                        return Results.Ok();
+
+                    default:
+                        // Just send default responses
+                        return WellKnownStateHandler.HandleWellKnownState(state);
+                }
+            })
+                .Produces<string>()
+                .WithEndpointMetadata(endpoint, d =>
+                {
+                    d.Append(new MarkdownHeader("SAP Get z_adds_mat_info  ", 3));
+                    d.Append(new MarkdownParagraph("Get SAP z_adds_mat_info details return 200"));
+                });
+    }
+
+}
---
+++ mock/UKHO.ADDS.Mocks.ERP/Override/Mocks/sap/PostZAaddsRos.cs
@@ -0,0 +1,39 @@
+﻿using UKHO.ADDS.Mocks.Markdown;
+using UKHO.ADDS.Mocks.Mime;
+using UKHO.ADDS.Mocks.States;
+
+namespace UKHO.ADDS.Mocks.Configuration.Mocks.sap
+{
+    public class PostZAaddsRos : ServiceEndpointMock
+    {
+        public override void RegisterSingleEndpoint(IEndpointMock endpoint) =>
+            endpoint.MapPost("/z_adds_ros.asmx", (HttpRequest request) =>
+            {
+                var state = GetState(request);
+
+                switch (state)
+                {
+                    case WellKnownState.Default:
+
+                        var pathResult = endpoint.GetFile("response.xml");
+
+                        if (pathResult.IsSuccess(out var file))
+                        {
+                            return Results.File(file.Path, MimeType.Text.Xml);
+                        }
+
+                        return Results.NotFound("Could not response xml");
+
+                    default:
+                        // Just send default responses
+                        return WellKnownStateHandler.HandleWellKnownState(state);
+                }
+            })
+            .Produces<string>()
+            .WithEndpointMetadata(endpoint, d =>
+            {
+                d.Append(new MarkdownHeader("SAP Post to z_adds_ros.asmx ", 3));
+                d.Append(new MarkdownParagraph("return 200 with the xml body"));
+            });
+    }
+}
