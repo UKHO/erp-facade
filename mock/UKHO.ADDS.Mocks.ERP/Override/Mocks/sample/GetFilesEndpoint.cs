1b51367a049babb1cd682c85302aa4bb80598ee2
rushdynajath <rushdy.najath@ukho.gov.uk>
2025-06-10 12:59:16 +0100

    clean up

--- mock/UKHO.ADDS.Mocks.ERP/Override/Mocks/sample/GetFilesEndpoint.cs
+++ mock/UKHO.ADDS.Mocks.ERP/Override/Mocks/sample/GetFilesEndpoint.cs
@@ -8,47 +8,36 @@ namespace UKHO.ADDS.Mocks.SampleService.Override.Mocks.sample
     {
         public override void RegisterSingleEndpoint(IEndpointMock endpoint) =>
             endpoint.MapGet("/files", (HttpRequest request) =>
+            {
+                var state = GetState(request);
+
+                switch (state)
                 {
-                    var state = GetState(request);
+                    case WellKnownState.Default:
+                        // ADDS Mock will have the 'default' state unless we have told it otherwise
+                        return Results.Ok("This is a result");
 
-                    switch (state)
-                    {
-                        case WellKnownState.Default:
-                            // ADDS Mock will have the 'default' state unless we have told it otherwise
-                            return Results.Ok("This is a result, just needed this text with the 200 response");
+                    case "get-file":
 
-                        case "get-file":
+                        var pathResult = GetFile("readme.txt");
 
-                            var pathResult = GetFile("readme.txt");
+                        if (pathResult.IsSuccess(out var file))
+                        {
+                            return Results.File(file.Open(), file.MimeType);
+                        }
 
-                            if (pathResult.IsSuccess(out var file))
-                            {
-                                return Results.File(file.Open(), MimeType.Text.Plain);
-                            }
+                        return Results.NotFound("Could not find the path in the /files GET method");
 
-                            return Results.NotFound("Could not find the path in the /files GET method");
-
-                        case "get-jpeg":
-
-                            var jpegPathResult = GetFile("messier-78.jpg");
-
-                            if (jpegPathResult.IsSuccess(out var jpegFile))
-                            {
-                                return Results.File(jpegFile.Open(), MimeType.Image.Jpeg);
-                            }
-
-                            return Results.NotFound("Could not find the JPEG path in the /files GET method");
-
-
-                        default:
-                            // Just send default responses
-                            return WellKnownStateHandler.HandleWellKnownState(state);
-                    }
-                })
+                    default:
+                        // Just send default responses
+                        return WellKnownStateHandler.HandleWellKnownState(state);
+                }
+            })
+                .Produces<string>()
                 .WithEndpointMetadata(endpoint, d =>
                 {
                     d.Append(new MarkdownHeader("Gets a file", 3));
-                    d.Append(new MarkdownParagraph("Try out the get-jpeg state!"));
+                    d.Append(new MarkdownParagraph("Just a demo method, nothing too exciting"));
                 });
     }
 }
