a479318144f6e8fa2c8b563296b19e86ba33036c
DanOwenUKHO <118909036+DanOwenUKHO@users.noreply.github.com>
2025-08-21 14:42:41 +0100

    Revert change

--- Deployment/import.tf
+++ Deployment/import.tf
@@ -1,4 +1,4 @@
 import {
   to = module.key_vault.azurerm_key_vault.kv
-  id = "/subscriptions/01d91561-74ff-4492-b37c-1806a21423c6/resourceGroups/erpfacade-vni-rg/providers/Microsoft.KeyVault/vaults/${local.key_vault_name}"  
+  id = "${azurerm_resource_group.rg.id}/providers/Microsoft.KeyVault/vaults/${local.key_vault_name}"  
 }
