241496472edb837751237785659deaa6464eb850
MattWin90 <matthew.winstanley@mastek.com>
2026-07-01 11:04:18 +0100

    Added version to all providers

--- Deployment/azure.tf
+++ Deployment/azure.tf
@@ -8,16 +8,19 @@ terraform {
 
 provider "azurerm" {
   features {}
+  version              = ">= 4.54.0"
 }
 
 provider "azurerm" {
   features {} 
   alias = "hub"
   subscription_id = var.hub_subscription_id
+  version              = ">= 4.54.0"
 }
 
 provider "azurerm" {
   features {} 
   alias = "erp"
   subscription_id = var.subscription_id
+  version              = ">= 4.54.0"
 }
